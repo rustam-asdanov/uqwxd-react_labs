eeaae463c333430e4669aac1a1adc0399d5bdb35
rustam-asdanov <asdanovrustam@gmail.com>
2023-09-21 04:47:06 -0400

    add and delete task

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -4,24 +4,67 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   
   // Add the handlesubmit code here
-  
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+
+    let todo = document.getElementById("todoAdd").value;
+    const newTodo = {
+        id: new Date().getTime(),
+        text: todo.trim(),
+        completed: false
+    };
+
+    if(newTodo.text.length > 0){
+        setTodos([...todos].concat(newTodo));
+    } else {
+        alert("Enter Valid Task");
+    }
+
+    document.getElementById("todoAdd").value = "";
+  }
   
   // Add the deleteToDo code here
-
+  const deleteTodo = (id) =>{
+   let updatedTodos = [...todos].filter((todo)=>todo.id !== id);
+   setTodos(updatedTodos);
+  }
   
   // Add the toggleComplete code here
+  const toggleComplete = (id)=>{
+      let updatedTodos = [...todos].map((todo)=>{
+          if(todo.id === id){
+              todo.completed = !todo.completed;
+          }
+          return todo;
+      })
 
+      setTodos(updatedTodos);
+  }
   
   // Add the submitEdits code here
 
   
 return(
-<div className ="App">
+<div className ="App" id="todo-list">
 <h1>Todo List</h1>
-<form>
+<form onSubmit={handleSubmit}>
 <input type ="text" align ="right" id= 'todoAdd'/>
 <button type ="submit">Add Todo</button>
 </form>
+{
+    todos.map((todo)=><div className="todo" key={todo.id}>
+        <div className="todo-text">
+            {todo.text}
+            <input 
+                type="checkbox" 
+                id="completed" 
+                checked={todo.completed} 
+                onChange={()=>toggleComplete(todo.id)}
+            />
+        </div>
+        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+    </div>)
+}
 </div>
 );
 };
